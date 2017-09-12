8b396038706e50db35ad4d3492ad50b0eaf90911
geasseur <baptiste.ventura@neuf.fr>
2017-09-12 11:09:20 +0200

    formulaire update

--- formulaire.php
+++ formulaire.php
@@ -14,10 +14,31 @@
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
   </head>
   <body>
-    <form class="" action="secret.php" method="post">
+    <form class="" action="formulaire.php" method="post">
       <label for="">entre votre mot de passe</label>
       <input type="text" name="codeTest" value="">
       <input type="submit" name="valider">
     </form>
+    <?php
+      $code = "kangourou";
+      if (isset($_POST["codeTest"])) {
+        $testCode = $_POST["codeTest"];
+          if ($testCode == $code) {
+            ?>
+            <h1>Voici les codes d'accès :</h1>
+          <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>
+
+          <p>
+          Cette page est réservée au personnel de la NASA. N'oubliez pas de la visiter régulièrement car les codes d'accès sont changés toutes les semaines.<br />
+          La NASA vous remercie de votre visite.
+          </p>
+            <?php
+          }
+          else{
+            echo "perdu ^_^";
+          }
+          
+      }
+     ?>
   </body>
 </html>
